0758f6edf4ab6ac186ece0c191a2877baee04645
Emil Pavlov <epavlov@hopper.com>
2020-02-24 13:37:14 +0200

    Improve file reading

--- util.py
+++ util.py
@@ -2,17 +2,12 @@
 
 
 def read_input(file, separator="\n"):
-    rd = open("input/" + file, "r")
+    with open("input/" + file, "r") as rd:
+        out = []
+        for line in rd:
+            for elem in line.strip().split(separator):
+                out.append(elem)
 
-    out = []
-    while True:
-        line = rd.readline()
-        if not line:
-            break
-        for elem in line.strip().split(separator):
-            out.append(elem)
-
-    rd.close()
     return out
 
 
